eb5cdad2de31138bb98165a2d35be75e0afb64a4
Ergun Acikoz <ergunackz@gmail.com>
2024-01-17 22:35:46 +0300

    Updating hw0b skeleton

---
+++ hw0b/src/JavaExercises.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaExercises {
+
+    /** Returns an array [1, 2, 3, 4, 5, 6] */
+    public static int[] makeDice() {
+        // TODO: Fill in this function.
+        return null;
+    }
+
+    /** Returns the order depending on the customer.
+     *  If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
+     *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
+     *  In any other case, return an empty String[] of size 3. */
+    public static String[] takeOrder(String customer) {
+        // TODO: Fill in this function.
+        return null;
+    }
+
+    /** Returns the positive difference between the maximum element and minimum element of the given array.
+     *  Assumes array is nonempty. */
+    public static int findMinMax(int[] array) {
+        // TODO: Fill in this function.
+        return 0;
+    }
+
+    /**
+      * Uses recursion to compute the hailstone sequence as a list of integers starting from an input number n.
+      * Hailstone sequence is described as:
+      *    - Pick a positive integer n as the start
+      *        - If n is even, divide n by 2
+      *        - If n is odd, multiply n by 3 and add 1
+      *    - Continue this process until n is 1
+      */
+    public static List<Integer> hailstone(int n) {
+        return hailstoneHelper(n, new ArrayList<>());
+    }
+
+    private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
+        // TODO: Fill in this function.
+        return null;
+    }
+
+}
---
+++ hw0b/src/ListExercises.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class ListExercises {
+
+    /** Returns the total sum in a list of integers */
+	public static int sum(List<Integer> L) {
+        // TODO: Fill in this function.
+        return 0;
+    }
+
+    /** Returns a list containing the even numbers of the given list */
+    public static List<Integer> evens(List<Integer> L) {
+        // TODO: Fill in this function.
+        return null;
+    }
+
+    /** Returns a list containing the common item of the two given lists */
+    public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
+        // TODO: Fill in this function.
+        return null;
+    }
+
+
+    /** Returns the number of occurrences of the given character in a list of strings. */
+    public static int countOccurrencesOfC(List<String> words, char c) {
+        // TODO: Fill in this function.
+        return 0;
+    }
+}
