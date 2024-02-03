ba0ab9e62e050259c4bb0a325bb511847d86471f
Noah Adhikari <noahadhikari@gmail.com>
2024-02-03 11:45:20 -0800

    Updating proj1b skeleton

---
+++ proj1b/tests/ArrayDeque61BPreconditionTest.java
@@ -0,0 +1,21 @@
+import jh61b.utils.Reflection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class ArrayDeque61BPreconditionTest {
+
+    @Test
+    @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+    void noNonTrivialFields() {
+        List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                .toList();
+
+        assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
+}
