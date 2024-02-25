98fcb6a99f10b06d012b1b412a8eb563e387a32d
Ergun Acikoz <ergunaczk@berkeley.edu>
2024-02-25 12:59:13 -0800

    Updating proj2a skeleton

---
+++ proj2a/src/utils/Utils.java
@@ -0,0 +1,19 @@
+package utils;
+
+/** A collection of file paths to data files. Make sure your data folder is on the same
+ *  level as "src".
+ */
+public class Utils {
+    private static final String PREFIX = "./data/ngrams/";
+
+    public static final String TOP_14337_WORDS_FILE = PREFIX + "top_14377_words.csv";
+
+    public static final String TOP_49887_WORDS_FILE = PREFIX + "top_49887_words.csv";
+
+    public static final String Q_WORDS_FILE = PREFIX + "words_that_start_with_q.csv";
+
+    public static final String SHORT_WORDS_FILE = PREFIX + "very_short.csv";
+    public static final String TOTAL_COUNTS_FILE = PREFIX + "total_counts.csv";
+
+}
+
