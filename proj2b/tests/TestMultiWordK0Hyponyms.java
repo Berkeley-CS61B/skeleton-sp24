96d058e16fc8f16b06747c14643a06851ced3cb7
Ergun Acikoz <ergunaczk@berkeley.edu>
2024-03-08 16:42:09 -0800

    Updating proj2b skeleton

---
+++ proj2b/tests/TestMultiWordK0Hyponyms.java
@@ -0,0 +1,37 @@
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import browser.NgordnetQueryType;
+import main.AutograderBuddy;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/** Tests the case where the list of words is length greater than 1, but k is still zero. */
+public class TestMultiWordK0Hyponyms {
+    // this case doesn't use the NGrams dataset at all, so the choice of files is irrelevant
+    public static final String WORDS_FILE = "data/ngrams/very_short.csv";
+    public static final String TOTAL_COUNTS_FILE = "data/ngrams/total_counts.csv";
+    public static final String SMALL_SYNSET_FILE = "data/wordnet/synsets16.txt";
+    public static final String SMALL_HYPONYM_FILE = "data/wordnet/hyponyms16.txt";
+    public static final String LARGE_SYNSET_FILE = "data/wordnet/synsets.txt";
+    public static final String LARGE_HYPONYM_FILE = "data/wordnet/hyponyms.txt";
+
+    /** This is an example from the spec.*/
+    @Test
+    public void testOccurrenceAndChangeK0() {
+        NgordnetQueryHandler studentHandler = AutograderBuddy.getHyponymsHandler(
+                WORDS_FILE, TOTAL_COUNTS_FILE, SMALL_SYNSET_FILE, SMALL_HYPONYM_FILE);
+        List<String> words = List.of("occurrence", "change");
+
+        NgordnetQuery nq = new NgordnetQuery(words, 0, 0, 0, NgordnetQueryType.HYPONYMS);
+        String actual = studentHandler.handle(nq);
+        String expected = "[alteration, change, increase, jump, leap, modification, saltation, transition]";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    // TODO: Add more unit tests (including edge case tests) here.
+
+    // TODO: Create similar unit test files for the k != 0 cases.
+}
