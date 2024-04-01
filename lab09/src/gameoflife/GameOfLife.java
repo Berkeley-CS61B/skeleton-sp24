26df6f9aa365edd07f6702ec1c5f01185c7d84f9
Jasmine Lin <jaslin@berkeley.edu>
2024-04-01 09:24:42 -0700

    Updating lab09 skeleton

---
+++ lab09/src/gameoflife/GameOfLife.java
@@ -0,0 +1,330 @@
+package gameoflife;
+
+import edu.princeton.cs.algs4.StdDraw;
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+import utils.FileUtils;
+
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+/**
+ * Am implementation of Conway's Game of Life using StdDraw.
+ * Credits to Erik Nelson, Jasmine Lin and Elana Ho for
+ * creating the assignment.
+ */
+public class GameOfLife {
+
+    private static final int DEFAULT_WIDTH = 50;
+    private static final int DEFAULT_HEIGHT = 50;
+    private static final String SAVE_FILE = "src/save.txt";
+    private long prevFrameTimestep;
+    private TERenderer ter;
+    private Random random;
+    private TETile[][] currentState;
+    private int width;
+    private int height;
+
+    /**
+     * Initializes our world.
+     * @param seed
+     */
+    public GameOfLife(long seed) {
+        width = DEFAULT_WIDTH;
+        height = DEFAULT_HEIGHT;
+        ter = new TERenderer();
+        ter.initialize(width, height);
+        random = new Random(seed);
+        TETile[][] randomTiles = new TETile[width][height];
+        fillWithRandomTiles(randomTiles);
+        currentState = randomTiles;
+    }
+
+    /**
+     * Constructor for loading in the state of the game from the
+     * given filename and initializing it.
+     * @param filename
+     */
+    public GameOfLife(String filename) {
+        this.currentState = loadBoard(filename);
+        ter = new TERenderer();
+        ter.initialize(width, height);
+    }
+
+    /**
+     * Constructor for loading in the state of the game from the
+     * given filename and initializing it. For testing purposes only, so
+     * do not modify.
+     * @param filename
+     */
+    public GameOfLife(String filename, boolean test) {
+        this.currentState = loadBoard(filename);
+    }
+
+    /**
+     * Initializes our world without using StdDraw. For testing purposes only,
+     * so do not modify.
+     * @param seed
+     */
+    public GameOfLife(long seed, boolean test) {
+        width = DEFAULT_WIDTH;
+        height = DEFAULT_HEIGHT;
+        random = new Random(seed);
+        TETile[][] randomTiles = new TETile[width][height];
+        fillWithRandomTiles(randomTiles);
+        currentState = randomTiles;
+    }
+
+    /**
+     * Initializes our world with a given TETile[][] without using StdDraw.
+     * For testing purposes only, so do not modify.
+     * @param tiles
+     * @param test
+     */
+    public GameOfLife(TETile[][] tiles, boolean test) {
+        TETile[][] transposeState = transpose(tiles);
+        this.currentState = flip(transposeState);
+        this.width = tiles[0].length;
+        this.height = tiles.length;
+    }
+
+    /**
+     * Flips the matrix along the x-axis.
+     * @param tiles
+     * @return
+     */
+    private TETile[][] flip(TETile[][] tiles) {
+        int w = tiles.length;
+        int h = tiles[0].length;
+
+        TETile[][] rotateMatrix = new TETile[w][h];
+        int y = h - 1;
+        for (int j = 0; j < h; j++) {
+            for (int i = 0; i < w; i++) {
+                rotateMatrix[i][y] = tiles[i][j];
+            }
+            y--;
+        }
+        return rotateMatrix;
+    }
+
+    /**
+     * Transposes the tiles.
+     * @param tiles
+     * @return
+     */
+    private TETile[][] transpose(TETile[][] tiles) {
+        int w = tiles[0].length;
+        int h = tiles.length;
+
+        TETile[][] transposeState = new TETile[w][h];
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                transposeState[x][y] = tiles[y][x];
+            }
+        }
+        return transposeState;
+    }
+
+    /**
+     * Runs the game. You don't have to worry about how this method works.
+     * DO NOT MODIFY THIS METHOD!
+     */
+    public void runGame() {
+        boolean paused = false;
+        long evoTimestamp = System.currentTimeMillis();
+        long pausedTimestamp = System.currentTimeMillis();
+        long clickTimestamp = System.currentTimeMillis();
+        while (true) {
+            if (!paused && System.currentTimeMillis() - evoTimestamp > 250) {
+                evoTimestamp = System.currentTimeMillis();
+                currentState = nextGeneration(currentState);
+            }
+            if (System.currentTimeMillis() - prevFrameTimestep > 17) {
+                prevFrameTimestep = System.currentTimeMillis();
+
+                double mouseX = StdDraw.mouseX();
+                double mouseY = StdDraw.mouseY();
+                int tileX = (int) mouseX;
+                int tileY = (int) mouseY;
+
+                TETile currTile = currentState[tileX % width][tileY % height];
+
+                if (StdDraw.isMousePressed() && System.currentTimeMillis() - clickTimestamp > 250) {
+                    clickTimestamp = System.currentTimeMillis();
+                    if (currTile == Tileset.CELL) {
+                        currentState[tileX][tileY] = Tileset.NOTHING;
+                    } else {
+                        currentState[tileX][tileY] = Tileset.CELL;
+                    }
+                }
+                if (StdDraw.isKeyPressed(KeyEvent.VK_SPACE) && System.currentTimeMillis() - pausedTimestamp > 500) {
+                    pausedTimestamp = System.currentTimeMillis();
+                    paused = !paused;
+                }
+                if (StdDraw.isKeyPressed(KeyEvent.VK_S)) {
+                    saveBoard();
+                    System.exit(0);
+                }
+                ter.renderFrame(currentState);
+            }
+        }
+    }
+
+
+    /**
+     * Fills the given 2D array of tiles with RANDOM tiles.
+     * @param tiles
+     */
+    public void fillWithRandomTiles(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = randomTile();
+            }
+        }
+    }
+
+    /**
+     * Fills the 2D array of tiles with NOTHING tiles.
+     * @param tiles
+     */
+    public void fillWithNothing(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    /**
+     * Selects a random tile, with a 50% change of it being a CELL
+     * and a 50% change of being NOTHING.
+     */
+    private TETile randomTile() {
+        // The following call to nextInt() uses a bound of 3 (this is not a seed!) so
+        // the result is bounded between 0, inclusive, and 3, exclusive. (0, 1, or 2)
+        int tileNum = random.nextInt(2);
+        return switch (tileNum) {
+            case 0 -> Tileset.CELL;
+            default -> Tileset.NOTHING;
+        };
+    }
+
+    /**
+     * Returns the current state of the board.
+     * @return
+     */
+    public TETile[][] returnCurrentState() {
+        return currentState;
+    }
+
+    /**
+     * At each timestep, the transitions will occur based on the following rules:
+     *  1.Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+     *  2.Any live cell with two or three neighbors lives on to the next generation.
+     *  3.Any live cell with more than three neighbors dies, as if by overpopulation,
+     *  4.Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+     * @param tiles
+     * @return
+     */
+    public TETile[][] nextGeneration(TETile[][] tiles) {
+        TETile[][] nextGen = new TETile[width][height];
+        // The board is filled with Tileset.NOTHING
+        fillWithNothing(nextGen);
+
+        // TODO: Implement this method so that the described transitions occur.
+        // TODO: The current state is represented by TETiles[][] tiles and the next
+        // TODO: state/evolution should be returned in TETile[][] nextGen.
+
+
+
+
+        // TODO: Returns the next evolution in TETile[][] nextGen.
+        return null;
+    }
+
+    /**
+     * Helper method for saveBoard without rendering and running the game.
+     * @param tiles
+     */
+    public void saveBoard(TETile[][] tiles) {
+        TETile[][] transposeState = transpose(tiles);
+        this.currentState = flip(transposeState);
+        this.width = tiles[0].length;
+        this.height = tiles.length;
+        saveBoard();
+    }
+
+    /**
+     * Saves the state of the current state of the board into the
+     * save.txt file (make sure it's saved into this specific file).
+     * 0 represents NOTHING, 1 represents a CELL.
+     */
+    public void saveBoard() {
+        // TODO: Save the dimensions of the board into the first line of the file.
+        // TODO: The width and height should be separated by a space, and end with "\n".
+
+
+
+        // TODO: Save the current state of the board into save.txt. You should
+        // TODO: use the provided FileUtils functions to help you. Make sure
+        // TODO: the orientation is correct! Each line in the board should
+        // TODO: end with a new line character.
+
+
+
+
+
+    }
+
+    /**
+     * Loads the board from filename and returns it in a 2D TETile array.
+     * 0 represents NOTHING, 1 represents a CELL.
+     */
+    public TETile[][] loadBoard(String filename) {
+        // TODO: Read in the file.
+
+        // TODO: Split the file based on the new line character.
+
+        // TODO: Grab and set the dimensions from the first line.
+
+        // TODO: Create a TETile[][] to load the board from the file into
+        // TODO: and any additional variables that you think might help.
+
+
+        // TODO: Load the state of the board from the given filename. You can
+        // TODO: use the provided builder variable to help you and FileUtils
+        // TODO: functions. Make sure the orientation is correct!
+
+
+
+
+        // TODO: Return the board you loaded. Replace/delete this line.
+        return null;
+    }
+
+    /**
+     * This is where we run the program. DO NOT MODIFY THIS METHOD!
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            // Read in the board from a file.
+            if (args[0].equals("-l")) {
+                GameOfLife g = new GameOfLife(args[1]);
+                g.runGame();
+            }
+            System.out.println("Verify your program arguments!");
+            System.exit(0);
+        } else {
+            long seed = args.length > 0 ? Long.parseLong(args[0]) : (new Random()).nextLong();
+            GameOfLife g = new GameOfLife(seed);
+            g.runGame();
+        }
+    }
+}
---
+++ lab09/tests/GameOfLifeTests.java
@@ -0,0 +1,225 @@
+import gameoflife.GameOfLife;
+import org.junit.Test;
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.fail;
+
+public class GameOfLifeTests {
+    private static final String SAVE_FILE = "src/save.txt";
+    private static final String SAVE_TEST = "tests/testFiles/saveTest.txt";
+    private static final String LOAD_TEST = "tests/testFiles/loadTest.txt";
+
+    private static final String BLANK = "patterns/blank.txt";
+    private static final String GLIDERGUN = "patterns/glidergun.txt";
+    private static final String HAMMERHEAD = "patterns/hammerhead.txt";
+    private static final String PENTADECATHLON = "patterns/pentadecathlon.txt";
+
+    /**
+     * Compares the two states passed in.
+     * @param student
+     * @param expected
+     */
+    private void checkState(TETile[][] student, TETile[][] expected) {
+        for (int i = 0; i < student.length; i++) {
+            for (int j = 0; j < student[0].length; j++) {
+                assertThat(student[i][j].description()).isEqualTo(expected[i][j].description());
+            }
+        }
+    }
+
+    /**
+     * Checks for the state of the world before and after a single generation,
+     * with dimensions of 3 by 3.
+     */
+    @Test
+    public void oneGeneration() {
+        checkIfPatternFilesAreModified();
+        TETile[][] testInput = new TETile[][] {
+                {Tileset.CELL, Tileset.CELL, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING}
+        };
+
+        TETile[][] result = new TETile[][] {
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING}
+        };
+
+        GameOfLife student = new GameOfLife(testInput, true);
+
+        // Generate next state from the provided input.
+        TETile[][] nextState = student.nextGeneration(testInput);
+
+        // Height and width need to stay the same before and after the generation.
+        assertThat(testInput.length).isEqualTo(nextState.length);
+        assertThat(testInput[0].length).isEqualTo(nextState[0].length);
+
+        // Check that the next generation state is as expected.
+        checkState(nextState, result);
+    }
+
+    /**
+     * Checks the state of the world after multiple generations.
+     */
+    @Test
+    public void multipleGenerations() {
+        checkIfPatternFilesAreModified();
+        TETile[][] testInput = new TETile[][] {
+                {Tileset.CELL, Tileset.CELL, Tileset.CELL, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING, Tileset.CELL}
+        };
+        GameOfLife student = new GameOfLife(testInput, true);
+        // Generate next state from the provided input.
+        TETile[][] firstState = student.nextGeneration(testInput);
+
+        /**
+         * 4 4
+         * 0111
+         * 1001
+         * 0001
+         * 0000
+         */
+        TETile[][] generationOne = new TETile[][] {
+                {Tileset.NOTHING, Tileset.CELL, Tileset.CELL, Tileset.CELL},
+                {Tileset.CELL, Tileset.NOTHING, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING}
+        };
+        checkState(firstState, generationOne);
+
+        /**
+         * 4 4
+         * 0111
+         * 0101
+         * 0000
+         * 0000
+         */
+        TETile[][] secondState = student.nextGeneration(firstState);
+        TETile[][] generationTwo = new TETile[][] {
+                {Tileset.NOTHING, Tileset.CELL, Tileset.CELL, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING}
+        };
+        checkState(secondState, generationTwo);
+
+        /**
+         * 4 4
+         * 0101
+         * 0101
+         * 0000
+         * 0000
+         */
+        TETile[][] thirdState = student.nextGeneration(secondState);
+        TETile[][] generationThree = new TETile[][] {
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING, Tileset.CELL},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING}
+        };
+        checkState(thirdState, generationThree);
+    }
+
+    /**
+     * Check for the save format, without generation of the next state, using a random seed
+     * as the initial state.
+     * @throws IOException
+     */
+    @Test
+    public void onlySave() throws IOException {
+        checkIfPatternFilesAreModified();
+        /**
+         * Should expect the board to be saved like in the following (not including dimensions):
+         * 010
+         * 011
+         * 100
+         * 010
+         */
+        TETile[][] result = new TETile[][] {
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.CELL, Tileset.CELL},
+                {Tileset.CELL, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.CELL, Tileset.NOTHING}
+        };
+
+        GameOfLife student = new GameOfLife(result, true);
+        student.saveBoard();
+
+        byte[] f1 = Files.readAllBytes(Paths.get(SAVE_FILE));
+        byte[] f2 = Files.readAllBytes(Paths.get(SAVE_TEST));
+
+        assertWithMessage("Checks that saving works and is as expected with the given seed.")
+                .that(Arrays.equals(f1, f2)).isTrue();
+    }
+
+    /**
+     * Check that loading works and matches expected output.
+     * @throws IOException
+     */
+    @Test
+    public void onlyLoad() throws IOException {
+        checkIfPatternFilesAreModified();
+        GameOfLife student = new GameOfLife(1234567, true);
+
+        TETile[][] result = new TETile[][] {
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING},
+                {Tileset.NOTHING, Tileset.NOTHING, Tileset.NOTHING}
+        };
+        TETile[][] loadResult = student.loadBoard(LOAD_TEST);
+        checkState(loadResult, result);
+    }
+
+    /**
+     * This checks if any of the pattern files have been modified. Modifications include
+     * adding a newline, deleting/replacing characters, adding characters, etc.
+     * DO NOT MODIFY ANYTHING IN THE FILE (otherwise testing will be harder).
+     */
+    private static void checkIfPatternFilesAreModified() {
+        if (hashFile("boba", BLANK) % 404 != -172) {
+            fail("blank.txt has been modified, please restore it back to the original version");
+        }
+        if (hashFile("panda express", GLIDERGUN) % 404 != -92) {
+            fail("glidergun.txt has been modified, please restore it back to the original version");
+        }
+        if (hashFile("rickroll", HAMMERHEAD) % 404 != -252) {
+            fail("hammerhead.txt has been modified, please restore it back to the original version");
+        }
+        if (hashFile("brrrrrr", PENTADECATHLON) % 404 != 380) {
+            fail("pentadecathlon.txt has been modified. Please restore it to the original version.");
+        }
+        if (hashFile("stop", SAVE_TEST) % 404 != 262) {
+            fail("saveTest.txt has been modified. Please restore it to the original version.");
+        }
+        if (hashFile("watch", LOAD_TEST) % 404 != -75) {
+            fail("loadTest.txt has been modified. Please restore it to the original version.");
+        }
+    }
+
+    private static int hashFile(String delimiter, String file) {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(file);
+        } catch (IOException e) {
+            System.err.println("File does not exist: " + file);
+            return 0;
+        }
+        BufferedReader br = new BufferedReader(fileReader);
+        List<String> contents = br.lines().toList();
+        return String.join(delimiter, contents).hashCode();
+    }
+}
